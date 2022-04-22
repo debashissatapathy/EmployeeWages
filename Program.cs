5bb829752f7db7525aed2f79b5bf969a4cdd9395
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:32:06 +0530

    Fetched

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Wages
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            UC6 u = new UC6();
+            u.Total_Time();
+        }
+    }
+}
