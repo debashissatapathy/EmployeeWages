3be275577657b37a65ed03e57dc34639627deea6
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 13:39:33 +0530

    Fetched

---
+++ UC5_MonthSalary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wages
+{
+    class UC5
+    {
+        public void Total_Time()
+        {
+            int empRateperHr = 20;
+            int fullDayHr = 8;
+            int Working_Day = 20;
+            int per_mon_sal_full = (empRateperHr * fullDayHr * Working_Day);
+            int per_mon_sal_part = (empRateperHr * (fullDayHr / 2) * Working_Day);
+            int i;
+            Console.WriteLine("Job Type For Monthly salary :");
+            string empType = Console.ReadLine();
+            switch (empType)
+            {
+                case "FullTime":
+                    Console.WriteLine("MOnthly salary for full Time :" + per_mon_sal_full);
+                    break;
+                case "PartTime":
+                    Console.WriteLine("MOnthly salary for part Time :" + per_mon_sal_part);
+                    break;
+            }
+        }
+    }
+}
